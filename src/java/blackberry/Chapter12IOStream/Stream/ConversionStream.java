e352c5443cf813e9b388fe211fc7bb019a8a6acb
867324154@qq.com <cqupt4031>
2021-03-24 22:19:58 +0800

    Buffer Stream

---
+++ src/java/blackberry/Chapter12IOStream/Stream/ConversionStream.java
@@ -0,0 +1,31 @@
+package blackberry.Chapter12IOStream.Stream;
+
+import org.junit.Test;
+
+/**
+ *  处理流二：转换流的使用
+ *  1、转换流属于字符流
+ *      InputStreamReader
+ *      OutputStreamWriter
+ *  2、作用：转换流提供了字节流和字符流之间的转换
+ *
+ */
+
+
+public class ConversionStream {
+    @Test
+    public void test1(){
+        /*
+            转换流：
+                转换流提供了字节流和字符流之间的转换
+                Java API 提供了两个转换流：
+                    InputStreamReader :将一个字节的InputStream转换为字符的Reader
+                    OutputStreamWriter：将一个字符的Writer转换为字节的OutputStream
+
+                字节流中的数据都是字符时，转换成字符操作更高效
+
+         */
+    }
+
+
+}
